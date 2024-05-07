2b75fca3c7b7508d49af03c27bf9995a3d7c0603
xuguosheng110 <xgs11@qq.com>
2024-05-07 22:53:55 +0800

    delete

---
+++ build/end/CMakeFiles/end.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/end.dir/src/end.cpp.o"
+  "end"
+  "end.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/end.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
